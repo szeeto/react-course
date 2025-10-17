6570973518531d8f7b53271209b79bf175934fac
Szeeto <patrasawali93@gmail.com>
2025-10-17 11:15:59 +0700

    update build bug

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './dist/css/main.css'
+import './styles/main.css'  // Updated import path
 
 createRoot(document.getElementById('root')).render(
   <>
